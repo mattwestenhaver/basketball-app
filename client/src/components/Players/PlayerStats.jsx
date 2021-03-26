aab849c1233d433d14ea7cc2ea43a3346d6bd7cb
Matthew Westenhaver <matthew.westenhaver@gmail.com>
2021-03-26 10:01:55 -0400

    Added sorting to player stats tble and some refactoring

--- client/src/components/Players/PlayerStats.jsx
+++ client/src/components/Players/PlayerStats.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { VscChromeClose } from 'react-icons/vsc'
 
+const statOptions = [
+    { key: 'pts', text: 'PTS'},
+    { key: 'ast', text: 'AST'},
+    { key: 'reb', text: 'REB'},
+    { key: 'stl', text: 'STL'},
+    { key: 'blk', text: 'BLK'},
+    { key: 'turnover', text: 'TO'},
+    { key: 'fg_pct', text: 'FG%'},
+    { key: 'fg3_pct', text: '3P%'},
+    { key: 'ft_pct', text: 'FT%'},
+]
+
 function PlayerStats({ players, removePlayer }) {
+    const [sortingStat, setSortingStat] = useState('pts')
+
     const handleRemovePlayer = (id) => {
         removePlayer(id)
     }
 
     const getPlayerString = (player) => {
         return `${player.info.first_name} ${player.info.last_name} ${player.info.position ? `(${player.info.position})` : ''} - ${player.info.team.abbreviation}`
+    }
+
+    const handleChangeSort = (stat) => {
+        setSortingStat(stat)
     }
 
     return (
@@ -20,17 +38,21 @@ function PlayerStats({ players, removePlayer }) {
                                 <tr className="table-labels">
                                     <th></th>
                                     <th>Player</th>
-                                    <th>PTS</th>
-                                    <th>AST</th>
-                                    <th>REB</th>
-                                    <th>STL</th>
-                                    <th>BLK</th>
-                                    <th>TO</th>
-                                    <th>FG%</th>
-                                    <th>3P%</th>
-                                    <th>FT%</th>
+                                    {statOptions.map((stat) => {
+                                        return (
+                                            <th
+                                                key={stat.key} 
+                                                onClick={() => handleChangeSort(stat.key)}
+                                                className={stat.key === sortingStat ? 'active-stat' : ''}
+                                            >
+                                                {stat.text}
+                                            </th>
+                                        )
+                                    })}
                                 </tr>
-                                {players.map((player, index) => {
+                                {players.sort((a, b) => {
+                                    return a.averages[sortingStat] > b.averages[sortingStat] ? -1 : 1
+                                }).map((player, index) => {
                                     return (
                                         <tr key={index} className="player-stats__row">
                                             <td>
